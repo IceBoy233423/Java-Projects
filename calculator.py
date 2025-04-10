d9aa142612970d633a9e1329b9b91d16bc0301ba
IceBoy233423 <isaiahstrack@gmail.com>
2025-04-10 16:19:46 -0500

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -13,16 +13,19 @@ def divide(num1, num2):
    return num1 / num2
  else:
    return "Error!"
+   # Defines algebraic properties of calculator
 
 while True:
   try: 
     num1 = int(input("Enter 1st Number (0 - 9):"))
     num2 = int(input("Enter 2nd Number (0 - 9):"))
+    #While 2 numbers aren't typed, input these 2 statements until 2 numbers from 0 to 9 are typed
     if num1 < 0 or num1 > 9 or num2 < 0 or num2 > 9:
       raise ValueError("Numbers should be between 0 and 9")
     break
   except ValueError as e:
     print("Invalid! Please enter a valid integer between 0 and 9.")
+    #If a number isn't from 0 to 9, restart the inputs
 
 while True:
   try:
@@ -32,6 +35,7 @@ while True:
     break
   except ValueError as e:
     print("Invalid! Please enter a valid operation.")
+    #If one of the 4 symbols shown isn't typed, then it must be typed again
 
 
 if operation == "+":
@@ -42,3 +46,4 @@ elif operation == "*":
   print(f"{num1} * {num2} = {multiply(num1, num2)}")
 elif operation == "/":
   print(f"{num1} / {num2} = {divide(num1, num2)}")
+  #determines which algebraic property to perform based off symbol
